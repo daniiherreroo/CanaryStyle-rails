daba78d8715950a1492d57b9745f86492dec790c
ElAlmeyda <garalmeidario@gmail.com>
2021-05-03 12:29:01 +0100

    creada tabla users

--- db/migrate/20210503103326_create_user.rb
+++ db/migrate/20210503103326_create_user.rb
@@ -3,7 +3,7 @@ class CreateUser < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name
       t.string :passwd
-      t:string :email
+      t.string :email
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_101543) do
+ActiveRecord::Schema.define(version: 2021_05_03_103326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,14 @@ ActiveRecord::Schema.define(version: 2021_05_03_101543) do
     t.string "name"
     t.string "price"
     t.string "source"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "passwd"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
